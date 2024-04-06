2d5517bfb2bb7ab00fabd3565995774a5621c60a
bobbyhadz <opmftw@gmail.com>
2024-04-06 14:59:10 +0300

    minor changes

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+export default function App() {
+  return (
+    <div>
+      <a
+        href="https://bobbyhadz.com"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <button>Click</button>
+      </a>
+    </div>
+  );
+}
